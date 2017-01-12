24b45c7853eb89f844d9191b7c7003f617ba3093
Yunfan <thefoggycity@yeah.net>
2017-01-12 15:21:39 +0800

    Added user interface.

--- WebcastFetching/Program.cs
+++ WebcastFetching/Program.cs
@@ -11,13 +11,109 @@ namespace WebcastFetching
     {
         static void Main(string[] args)
         {
-            //Uri uri = new Uri("https://ivle.nus.edu.sg:443/v1/Media/Student/Multimedia.aspx?CourseID=4ec0ae0f-0745-4308-b350-9bc27d941c48&ChannelID=5de9c813-3644-4162-95e7-e14a278d8946");
-            //Console.WriteLine(uri);
-            //Console.WriteLine(uri.IsDefaultPort);
-            //Console.ReadKey();
+            string DestPath = String.Empty, 
+                m3u8Src = String.Empty;
 
-            UrlHdl PlayerPage = new UrlHdl("https://vod.nus.edu.sg/hls-vod/ivle/users/13342cd6-97ce-4859-8185-5fe7b296ca56/TRAILER_0923_NO_INTERVIEW_NO_BG.mp4.m3u8");
-            string DestPath = Environment.CurrentDirectory + "\\trailer.mp4";
+            switch (args.Count())
+            {
+                case 0:
+                    string input;
+                    bool vaildflag;
+                    do
+                    {
+                        Console.WriteLine("Enter m3u8 web link:");
+                        input = Console.ReadLine().TrimEnd('\n');
+                    } while (!SrcHdl.IsVaildm3u8(input));
+                    m3u8Src = input;
+                    do
+                    {
+                        Console.WriteLine("Download to:");
+                        input = Console.ReadLine().TrimEnd('\n');
+                        vaildflag = true;
+                        if (input == String.Empty)
+                            input = Environment.CurrentDirectory + '\\' + SrcHdl.GetLinkFileNamem3u8(m3u8Src);
+                        else if ((input.Last() == '\\') || (input.Last() == '/'))    // Is directory
+                        {
+                            if (Directory.Exists(input))
+                                input += ('\\' + SrcHdl.GetLinkFileNamem3u8(m3u8Src));
+                            else
+                            {
+                                Console.WriteLine("Invaild path.");
+                                vaildflag = false;
+                                continue;
+                            }
+                        }
+                        else
+                        {
+                            if (!(input.Contains('\\') || input.Contains('/')))
+                            {
+                                input = Environment.CurrentDirectory + input;
+                            }
+                            else if (!Directory.Exists(input.Substring(0, input.LastIndexOf('\\'))))
+                            {
+                                Console.WriteLine("Invaild path.");
+                                vaildflag = false;
+                                continue;
+                            }
+                        }
+                    } while (!vaildflag);
+                    DestPath = input;
+                    break;
+
+                case 1:
+                    if (SrcHdl.IsVaildm3u8(args[0]))
+                        m3u8Src = args[0];
+                    else
+                    {
+                        Console.WriteLine("Invaild link.");
+                        Console.WriteLine("Usage:\twebcasfetching [Url_to_m3u8] [Download_destination]");
+                        Environment.Exit(0);
+                    }
+                    DestPath = Environment.CurrentDirectory + '\\' + SrcHdl.GetLinkFileNamem3u8(m3u8Src);
+                    break;
+
+                case 2:
+                    if (SrcHdl.IsVaildm3u8(args[0]))
+                        m3u8Src = args[0];
+                    else
+                    {
+                        Console.WriteLine("Invaild link.");
+                        Console.WriteLine("Usage:\twebcasfetching [Url_to_m3u8] [Download_destination]");
+                        Environment.Exit(0);
+                    }
+                    if ((args[1].Last() == '\\') || (args[1].Last() == '/'))    // Is directory
+                    {
+                        if (Directory.Exists(args[1]))
+                            DestPath = args[1] + '\\' + SrcHdl.GetLinkFileNamem3u8(m3u8Src);
+                        else
+                        {
+                            Console.WriteLine("Invaild path.");
+                            Console.WriteLine("Usage:\twebcasfetching [Url_to_m3u8] [Download_destination]");
+                            Environment.Exit(0);
+                        }
+                    }
+                    else
+                    {
+                        if (!(args[1].Contains('\\') || args[1].Contains('/')))
+                        {
+                            DestPath = Environment.CurrentDirectory + args[1];
+                        }
+                        else if (!Directory.Exists(args[1].Substring(0, args[1].LastIndexOf('\\'))))
+                        {
+                            Console.WriteLine("Invaild path.");
+                            Console.WriteLine("Usage:\twebcastfetching [Url_to_m3u8] [Download_destination]");
+                            Environment.Exit(0);
+                        }
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("Usage:\twebcastfetching [Url_to_m3u8] [Download_destination]");
+                    Environment.Exit(0);
+                    break;
+            }
+
+            UrlHdl PlayerPage = new UrlHdl(m3u8Src);
             string[] Urls;
             FileStream Dest = new FileStream(DestPath, FileMode.Append);
             if (Dest.CanWrite)
--- WebcastFetching/SrcHdl.cs
+++ WebcastFetching/SrcHdl.cs
@@ -8,9 +8,16 @@ namespace WebcastFetching
 {
     static class SrcHdl
     {
-        //public string Theoplayer()  //Used by ivle
-        //{
+        public static bool IsVaildm3u8(string Link)
+        {
+            return ((Link.Substring(Link.Count() - 4, 4).ToLower() == "m3u8")
+                && (Link.Substring(0, 4).ToLower() == "http"));
+            //  Link.EndsWith("m3u8",StringComparison.OrdinalIgnoreCase)
+        }
 
-        //}
+        public static string GetLinkFileNamem3u8(string Link)
+        {
+            return Link.Substring(Link.LastIndexOf('/') + 1, Link.LastIndexOf('.')  - Link.LastIndexOf('/') - 1);
+        }
     }
 }
